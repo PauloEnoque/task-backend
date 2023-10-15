aa337347ea2d56b89ed8d4b48690ab8d3ab1e56f
Paulo Enoque <paulo.enoque@cc-g.com>
2023-10-15 13:37:49 +0200

    Removed the built in Tomcat

--- src/main/kotlin/com/example/taskbackend/TaskBackendApplication.kt
+++ src/main/kotlin/com/example/taskbackend/TaskBackendApplication.kt
@@ -1,11 +1,22 @@
 package com.example.taskbackend
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class TaskBackendApplication
+class TaskBackendApplication : SpringBootServletInitializer(){
+    override fun configure(builder: SpringApplicationBuilder): SpringApplicationBuilder {
+        return builder.sources(TaskBackendApplication::class.java)
+    }
 
-fun main(args: Array<String>) {
-    runApplication<TaskBackendApplication>(*args)
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplication.run(TaskBackendApplication::class.java, *args)
+        }
+    }
 }
+
